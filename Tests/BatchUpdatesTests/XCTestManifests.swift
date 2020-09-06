00f56c08f885d42f7eba045e3264eb439f3fbd5a
Michael Nisi <michael.nisi@gmail.com>
2020-09-06 17:33:10 +0200

    Create new Swift package with Xcode

--- Tests/BatchUpdatesTests/XCTestManifests.swift
+++ Tests/BatchUpdatesTests/XCTestManifests.swift
@@ -2,8 +2,8 @@ import XCTest
 
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
-  return [
-    testCase(SectionedDataSourceTests.allTests),
-  ]
+    return [
+        testCase(BatchUpdatesTests.allTests),
+    ]
 }
 #endif
